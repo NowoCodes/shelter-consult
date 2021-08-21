cb58b8bd090b54a138f51fba122894e5ca12cd0c
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-21 07:09:45 +0100

    add files

---
+++ src/components/Hero.vue
@@ -0,0 +1,116 @@
+<template>
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active">
+            <div class="carousel-background slide-1"></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Shelter Consult</span></h2>
+                <p class="display-6 animate__animated animate__fadeInUp">GRAPHIC REPRESENTATION - SKYFIELD ESTATE AJAH,
+                  LAGOS</p>
+                <a href="#home" class="btn-get-started animate__animated animate__fadeInUp scrollto">Get Started</a>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 2 -->
+          <div class="carousel-item">
+            <div class="carousel-background slide-2"></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <p class="display-6 animate__animated animate__fadeInUp">CITN TAX ACADEMY, ALONG ILORIN-AJASE – EPO ROAD,
+                  KWARA STATE</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 3 -->
+          <div class="carousel-item">
+            <div class="carousel-background slide-3"></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <p class="display-6 animate__animated animate__fadeInUp">GRAPHICS REPRESENATTIONS - APARTMENT BUILDINGS, ALONG AINA STREET, AJAH, LAGOS</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 4 -->
+          <div class="carousel-item">
+            <div class="carousel-background slide-4"></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <p class="display-6 animate__animated animate__fadeInUp">STUDENTS’ HOSTEL ALONG COMMERCIAL AVENUE, SABO, YABA, LAGOS</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 5 -->
+          <div class="carousel-item">
+            <div class="carousel-background slide-5"></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <p class="display-6 animate__animated animate__fadeInUp">STUDENTS’ HOSTEL ALONG COMMERCIAL AVENUE, SABO, YABA, LAGOS</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 6 -->
+          <div class="carousel-item">
+            <div class="carousel-background slide-6"></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <p class="display-6 animate__animated animate__fadeInUp">SWISS INTERNATIONAL HOTEL, ABEOKUTA, OGUN STATE</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 7 -->
+          <div class="carousel-item">
+            <div class="carousel-background slide-7"></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <p class="display-6 animate__animated animate__fadeInUp">TAX PROFESSIONALS’ HOUSE, ALAUSA, IKEJA, LAGOS.</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 8 -->
+          <div class="carousel-item">
+            <div class="carousel-background slide-8"></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <p class="display-6 animate__animated animate__fadeInUp">NIGERIAN AIRSPACE MANAGEMENT AGENCY CORPORATE HEADQUATERS, ABUJA</p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon bi bi-chevron-double-left" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon bi bi-chevron-double-right" aria-hidden="true"></span>
+        </a>
+
+      </div>
+    </div>
+  </section>
+  <!-- End Hero -->
+</template>
+
+<script>
+export default {
+  name: "Hero"
+}
+</script>
---
+++ src/views/Contact.vue
@@ -0,0 +1,98 @@
+<template>
+  <!-- ======= Contact Us Section ======= -->
+  <section id="contact" class="contact section-bg">
+
+    <div class="container">
+      <div class="section-title">
+        <h2>Contact Us</h2>
+        <p>For any type of project please don't hesitate to get in touch with us.</p>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+
+      <div class="row">
+
+        <div class="col-lg-6 d-flex align-items-stretch infos">
+
+          <div class="row">
+
+            <div class="col-lg-6 info d-flex flex-column align-items-stretch">
+              <i class="bx bx-map"></i>
+              <h4>Address</h4>
+              <h4>Head Office:</h4>
+              <p>
+                Plot 11, Ajayi Bembe Street,<br>
+                off University of Lagos Road,<br>
+                Yaba, Lagos, Nigeria.<br>
+                P.O. Box 1, Ebute Metta,<br>
+                Lagos, Nigeria.
+              </p>
+            </div>
+            <div class="col-lg-6 info d-flex flex-column align-items-stretch">
+              <i class="bx bx-map"></i>
+              <h4>Address</h4>
+              <h4>Abuja Liaison Office:</h4>
+              <p>
+                Oyo House, 4th Floor,<br>
+                Opposite Federal Ministry of Finance,<br>
+                Central Business District, Abuja.<br>
+              </p>
+            </div>
+            <div class="col-lg-6 info info-bg d-flex flex-column align-items-stretch">
+              <i class="bx bx-phone"></i>
+              <h4>Call Us</h4>
+
+              <p>+234 802 328 6289 <br>+234 807 720 0082 <br>+234 908 003 1618</p>
+            </div>
+            <div class="col-lg-6 info info-bg d-flex flex-column align-items-stretch">
+              <i class="bx bx-envelope"></i>
+              <h4>Email Us</h4>
+              <p> info@shelterconsult-adcpm.com</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-lg-6 d-flex align-items-stretch contact-form-wrap">
+          <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+            <div class="row">
+              <div class="col-md-6 form-group">
+                <label for="name">Your Name</label>
+                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required="">
+              </div>
+              <div class="col-md-6 form-group mt-3 mt-md-0">
+                <label for="email">Your Email</label>
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required="">
+              </div>
+            </div>
+            <div class="form-group mt-3">
+              <label for="subject">Subject</label>
+              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required="">
+            </div>
+            <div class="form-group mt-3">
+              <label for="message">Message</label>
+              <textarea class="form-control" name="message" rows="8" required=""></textarea>
+            </div>
+            <div class="my-3">
+              <div class="loading">Loading</div>
+              <div class="error-message"></div>
+              <div class="sent-message">Your message has been sent. Thank you!</div>
+            </div>
+            <div class="text-center">
+              <button type="submit">Send Message</button>
+            </div>
+          </form>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Contact"
+}
+</script>
